24dac8d789b89b8429f000a99a62d3426038143f
tybrianna <tybriannahall@gmail.com>
2026-05-07 14:15:18 -0400

    added code lines to admin.tsx

---
+++ Dynamic-Routing/src/pages/Admin.tsx
@@ -0,0 +1,13 @@
+const Admin = () => {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+
+      <p>
+        Welcome to the Admin Dashboard.
+      </p>
+    </div>
+  );
+};
+
+export default Admin;
