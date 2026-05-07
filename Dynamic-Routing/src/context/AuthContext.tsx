0109fa863b9c7adf85ab30f84f9368fec20a4a73
tybrianna <tybriannahall@gmail.com>
2026-05-07 14:07:34 -0400

    completed step 3 of the requirements

---
+++ Dynamic-Routing/src/context/AuthContext.tsx
@@ -0,0 +1,59 @@
+import {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+} from "react";
+
+type AuthContextType = {
+  isAuthenticated: boolean;
+  login: () => void;
+  logout: () => void;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(
+  undefined
+);
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+export const AuthProvider = ({
+  children,
+}: AuthProviderProps) => {
+  const [isAuthenticated, setIsAuthenticated] =
+    useState(false);
+
+  const login = () => {
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error(
+      "useAuth must be used within AuthProvider"
+    );
+  }
+
+  return context;
+};
