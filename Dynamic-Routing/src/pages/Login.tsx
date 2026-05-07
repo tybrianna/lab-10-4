8a8bef94bdd6b01193b3c78981d05a46ccd03f6a
tybrianna <tybriannahall@gmail.com>
2026-05-07 14:14:43 -0400

    added code lines to login.tsx

---
+++ Dynamic-Routing/src/pages/Login.tsx
@@ -0,0 +1,25 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+const Login = () => {
+  const { login } = useAuth();
+
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    login();
+    navigate("/admin");
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <button onClick={handleLogin}>
+        Log In
+      </button>
+    </div>
+  );
+};
+
+export default Login;
